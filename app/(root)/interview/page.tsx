e47f1db9e58a360d4e82a448e44005028655fb72
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-03-29 10:41:34 +0300

    Add InterviewPage component with Agent integration

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import {Agent} from "@/components/Agent";
+
+const InterviewPage = () => {
+    return (
+        <>
+            <h3>Interview Generation</h3>
+            <Agent userName={"agent"} />
+        </>
+    );
+};
+
+export default InterviewPage;
