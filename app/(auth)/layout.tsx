3af1199f374aa848f767a2a2c643c96e2be138c6
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-03-29 08:35:30 +0300

    Add authentication check in AuthLayout to redirect authenticated users

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import {getCurrentUser} from "@/lib/actions/auth.actions";
+import {redirect} from "next/navigation";
 
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
+  const isAuthenticated = await getCurrentUser();
+
+  if (isAuthenticated) redirect('/');
   return (
     <div className='auth-layout'>
       {children}
