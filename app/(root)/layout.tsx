c80579360645679f95cbd1ee06bd57d5e3cd57e1
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-03-29 08:35:38 +0300

    Add authentication check in Layout to redirect unauthenticated users

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import {redirect} from 'next/navigation';
 import React, { ReactNode } from 'react'
+import {getCurrentUser} from "@/lib/actions/auth.actions";
 
-const Layout = ({ children }: { children: ReactNode }) => {
+const Layout = async ({ children }: { children: ReactNode }) => {
+    const isAuthenticated = await getCurrentUser();
+
+    if (!isAuthenticated) redirect('/sign-in');
   return (
     <div className='root-layout'>
       <nav>
