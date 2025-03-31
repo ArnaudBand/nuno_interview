386edad07f85e2fd795bf75738b36e5fb32c681a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-03-31 20:01:56 +0300

    Added gemini and vapi keys

--- env.ts
+++ env.ts
@@ -3,5 +3,11 @@ export const env = {
     projectId: String(process.env.FIREBASE_PROJECT_ID),
     key: String(process.env.FIREBASE_PRIVATE_KEY),
     email: String(process.env.FIREBASE_CLIENT_EMAIL)
+  },
+  google: {
+    key: String(process.env.GOOGLE_GENERATIVE_AI_API_KEY)
+  },
+  vapi: {
+    key: String(process.env.NEXT_PUBLIC_VAPI_API_KEY),
   }
 }
